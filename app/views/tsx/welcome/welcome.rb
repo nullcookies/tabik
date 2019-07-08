bd22867872e9b7373fd26017e06da676c077a52a
mmbug <mmbug256@gmail.com>
2019-07-08 15:24:21 -0400

    ./upload.sh

--- app/views/tsx/welcome/welcome.rb
+++ app/views/tsx/welcome/welcome.rb
@@ -1,4 +1,5 @@
-*#{@tsx_bot.title}* #{@tsx_bot.awards if !@tsx_bot.awards.nil?} #{Version.current}
+*#{@tsx_bot.title}* #{@tsx_bot.awards if !@tsx_bot.awards.nil?}
+`v#{Version.current}`
 
 Клиент #{icon('id')} *#{hb_client.id}*
 Баланс *#{@tsx_bot.amo(hb_client.available_cash)}*
@@ -12,6 +13,6 @@
       button("#{icon('moneybag')} Вывести заработанное", 'btc_cashout')
     ],
     [
-      share_button('Поделиться ботом', 'Регайся по моей ссылке :)')
+      share_button('Поделиться ботом', "Регайся по моей ссылке :) #{hb_client.make_referal_link(@tsx_bot)}")
     ]
 ]
