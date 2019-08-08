731e17b03d7fca26629375a5e84ffadbbd0242d1
mmbug <mmbug256@gmail.com>
2019-08-08 19:50:39 -0400

    ./upload.sh

--- app/views/tsx/admin/botstat.rb
+++ app/views/tsx/admin/botstat.rb
@@ -3,8 +3,8 @@
 Бот [@#{@tsx_bot.tele + (@tsx_bot.underscored_name == 1 ? "_bot": "bot")}](http://t.me/#{@tsx_bot.tele + (@tsx_bot.underscored_name == 1 ? "_bot": "bot")})
 Номер бота *#{@tsx_bot.id}*
 Клиентов *#{@tsx_bot.bot_clients.count}*
-Баланс *#{@tsx_bot.amo(@tsx_bot.beneficiary.rent_cash)}*
-К выплате *#{@tsx_bot.debt_in_btc} BTC*
+# Баланс *#{@tsx_bot.amo(@tsx_bot.beneficiary.rent_cash)}*
+# К выплате *#{@tsx_bot.debt_in_btc} BTC*
 Погашение *#{human_date(@tsx_bot.paid + 1.month)}*
 
 Всего *#{kladov(@tsx_bot.all_items)}*
