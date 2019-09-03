cfa979f275bed28c9c1c98bba26d37448d9967db
mmbug <mmbug256@gmail.com>
2019-09-03 14:30:04 -0400

    easy

--- jobs/easypay.rb
+++ jobs/easypay.rb
@@ -1,19 +1,17 @@
 require_relative './requires'
 require 'colorize'
 
-# DB = Sequel.connect('postgres://aijsnlqeczrjde:0841bee8fe4b53474a43e815c5b19952c74b7c8b428bacc73943388d678a274d@ec2-174-129-226-232.compute-1.amazonaws.com:5432/d2i3n5oehr5m5n')
-
 logger = CronLogger.new
 # DB.logger = logger
 
 def easypay_login(bot)
   i = 0
-  num = 3
+  num = 4
   logged = false
   while i < num  do
     i += 1
     puts "#{bot.title}:  Trying to solve reCaptcha #{i} try ..."
-    client = AntiCaptcha.new('7766d57328e2d81745bc87bcf2d6f765')
+    client = AntiCaptcha.new('397e590788cf7c2248374b889d395cbe')
     options = {
         website_key: '6LefhCUTAAAAAOQiMPYspmagWsoVyHyTBFyafCFb',
         website_url: 'https://partners.easypay.ua/auth/signin'
@@ -22,8 +20,8 @@ def easypay_login(bot)
       solution = client.decode_nocaptcha!(options)
       resp = solution.g_recaptcha_response
     rescue AntiCaptcha::Error => ex
-      puts "#{bot.title}: AntiCaptcha timeout. Next try.".red
       puts ex.message
+      puts "#{bot.title}: AntiCaptcha timeout. Next try.".red
       next
     end
     puts "#{bot.title}: Got AntiCaptcha endresponse: #{resp}".green
@@ -33,12 +31,12 @@ def easypay_login(bot)
     web.open_timeout = 10
     web.user_agent = "Mozilla/5.0 Gecko/20101203 Firefox/3.6.13"
     proxy = Prox.get_active
-    puts proxy.inspect
     web.agent.set_proxy(proxy.host, proxy.port, proxy.login, proxy.password)
     puts "#{bot.title}: Retrieving main page".white
     easy = web.get('https://partners.easypay.ua/auth/signin')
-    login = bot.payment_option('login', Meth::__easypay)
+    login = bot.payment_option('phone', Meth::__easypay)
     password = bot.payment_option('password', Meth::__easypay)
+    puts "Trying with proxy #{proxy.host}:#{proxy.host} #{proxy.login}/#{proxy.password}"
     puts "#{bot.title}: Trying to login with #{login}/#{password}".white
     begin
       # exit
@@ -53,7 +51,7 @@ def easypay_login(bot)
       puts e.backtrace.join("\t\n")
       next
     end
-    if logged.title != "EasyPay - Вход в систему"
+    if logged.title != "EasyPay - Вхiд до системи" and logged.title != "EasyPay - Вход в систему"
       puts "#{bot.title}: Logged to Easypay.".colorize(:green)
       logged = true
       return web
@@ -77,8 +75,9 @@ def easypay_login_nocaptcha(bot)
   web.agent.set_proxy(proxy.host, proxy.port, proxy.login, proxy.password)
   puts "#{bot.title}: Retrieving main page".white
   easy = web.get('https://partners.easypay.ua/auth/signin')
-  login = bot.payment_option('login', Meth::__easypay)
+  login = bot.payment_option('phone', Meth::__easypay)
   password = bot.payment_option('password', Meth::__easypay)
+  puts "Trying with proxy #{proxy.host}:#{proxy.host} #{proxy.login}/#{proxy.password}"
   puts "#{bot.title}: Trying to login with #{login}/#{password}".white
   begin
     # exit
@@ -105,6 +104,7 @@ def get_today_transactions(web, bot)
   puts "#{bot.title}: Checking all payments for the current day".white
   wallet = bot.payment_option('wallet', Meth::__easypay)
   st = web.get("https://partners.easypay.ua/wallets/buildreport?walletId=#{wallet}&month=#{Date.today.month}&year=#{Date.today.year}")
+  puts st.inspect
   tab = st.search(".//table[@class='table-layout']").children
   puts "#{bot.title}: TAB COUNT: #{tab.count}"
   Easypay.where(bot: bot.id).delete
@@ -113,12 +113,12 @@ def get_today_transactions(web, bot)
     to_match = ''
     amount = ''
     d.children.each do |td, td2|
-      puts td.inspect
+      #puts td.inspect
       if i == 2
         to_match << td.inner_text
       end
       if i == 6
-         amount = td.inner_text
+        amount = td.inner_text
       end
       if i == 10
         to_match << td.inner_text
@@ -129,15 +129,19 @@ def get_today_transactions(web, bot)
     matched = "#{to_match}".match(/.*(\d{2}:\d{2})\D*(\d+)/)
     if matched
       dat =  "#{to_match}".match(/(\d{2}.\d{2}.\d{4}).*/)
-      puts "Payment date: #{dat}"
       if Date.parse(dat.captures.first) < Date.today - 1.days
         puts "TODAY IS FINISHED. NOT SAVING THE REST".red
         return false
       end
       code = "#{matched.captures.first}#{matched.captures.last}"
       wallet = Wallet.find(bot: bot.id, active: 1)
-      puts "Adding payment #{amount} with code #{code}".colorize(:blue)
-      Easypay.create(wallet: wallet.id, bot: bot.id, code: code, amount: amount)
+      p = Easypay.where("bot = #{bot.id} and wallet = '#{wallet.id}' and  code = '#{code}' and amount = '#{amount}'")
+      if p.count == 0
+        puts "Adding payment #{amount} with code #{code}".colorize(:blue)
+        Easypay.create(wallet: wallet.id, bot: bot.id, code: code, amount: amount)
+      else
+        puts "Code #{code} already saved in database"
+      end
     else
       puts "NOT MATCHED".red
     end
