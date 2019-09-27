8e58fb4051d5f1914bcdc55da8955b284589cafa
mmbug <mmbug256@gmail.com>
2019-09-27 11:40:18 -0400

    ./upload.sh

--- app/views/tsx/hosting/card.rb
+++ app/views/tsx/hosting/card.rb
@@ -3,6 +3,10 @@
 Номер #{icon('id')} *#{@kladman.id}*
 Никнейм *#{@kladman.username}*
 Остатки *#{kladov(@kladman.rest - @kladman.klads_uploaded)}*
+Загружено *#{kladov(@kladman.klads_uploaded)}*
+Штрафов *#{@tsx_bot.amo(@kladman.kladman_fines)}*
+Выплачено *#{@tsx_bot.amo(@kladman.kladman_paid)}*
+К выплате *#{@tsx_bot.amo(@kladman.kladman_debt - @kladman.kladman_paid - @kladman.kladman_fines)}*
 ****
 [
     [
