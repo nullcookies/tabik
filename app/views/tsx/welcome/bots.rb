599821142894fe241a5aa6bbd2edf9dc0555ab63
mmbug <mmbug256@gmail.com>
2019-10-07 19:15:51 -0400

    ./upload.sh

--- app/views/tsx/welcome/bots.rb
+++ app/views/tsx/welcome/bots.rb
@@ -1,5 +1,7 @@
 #{bots_welcome_ukraine}
 #{bots_welcome_russia}
 #{bots_welcome_kaz}
+
+Пишите @pndsupport по вопросам аренды бота. Чтобы арендовать бот, нужно иметь ветку на одном из известных форумов.
 ****
 [[]]
