8a9eeb616675053e898fbdf5f97beb6ecdf8cf2e
mmbug <mmbug256@gmail.com>
2019-10-07 19:56:29 -0400

    ./upload.sh

--- app/views/tsx/welcome/bots.rb
+++ app/views/tsx/welcome/bots.rb
@@ -1,7 +1,16 @@
-#{bots_welcome_ukraine}
-#{bots_welcome_russia}
-#{bots_welcome_kaz}
+#{main_top}
+#{bots_welcome}
 
 #{icon('grey_exclamation')} По вопросам аренды бота пишите @pndsupport. Магазинам системы обязательно иметь иметь ветку на одном из известных форумов СНГ.
 ****
-[[]]
+[
+    [
+      button('Украина', 'bots_ukraine')
+    ],
+    [
+        button('Россия', 'bots_russia'),
+    ],
+    [
+        button('Казахстан', 'bots_kaz'),
+    ]
+]
