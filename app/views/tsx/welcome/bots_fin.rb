29395de3756fa8df26ac8e5421bdd4f4d1a3c4bc
mmbug <mmbug256@gmail.com>
2019-12-01 13:25:21 +0200

    Version bump to 10.129.0

---
+++ app/views/tsx/welcome/bots_fin.rb
@@ -0,0 +1,18 @@
+#{icon('books')} *Оптовые поставщики*
+В списке проверенные оптовые поставщики. Во избежание недоразумений, *пользуйтесь гарантом сделок* @pndsupport
+
+@optbosssetb Альфа, амф и шишки.
+@helpforobot Лучшие кристаллы в украине!
+[@macdrugs_supp](http://t.me/macdrugs_supp) опт и розница фен, шиш, A-PVP, мет, кокс
+@Optpapa опт и розница шишки
+[@Ramzes_labs_shop](http://t.me/Ramzes_labs_shop) опт и розница A-PVP, фен и шишки
+@ZPbizZP A-PVP, фен, шишки опт и розница
+
+#{icon('grey_exclamation')} Присоединяйтесь в [чат оптовых продаж](https://t.me/joinchat/OcmDBFSMrpIAEBCJzW1Mww). Размещайте там объявления один раз в день бесплатно.
+****
+[
+    [
+        button("#{icon('green_book')} Клады", 'bots_roznica'),
+        button("#{icon('books')} Опт", 'bots_opt')
+    ]
+]
