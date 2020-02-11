d3f612debd755b1356070c1d13c016ad3abc8768
User <user@Users-MacBook-Pro.local>
2020-02-11 17:16:20 +0200

    ./upload.sh

--- jobs/cx.rb
+++ jobs/cx.rb
@@ -4,9 +4,10 @@ logger = CronLogger.new
 logger.noise "Checking new stories ... "
 stories = eval(Faraday.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty').body)
 stories.each do |story|
-  puts "https://hacker-news.firebaseio.com/v0/item/#{story}.json?print=pretty"
+  # puts "https://hacker-news.firebaseio.com/v0/item/#{story}.json?print=pretty"
   story_body = Faraday.get("https://hacker-news.firebaseio.com/v0/item/#{story}.json?print=pretty").body
   res = JSON.parse(story_body)
+  next if res.nil?
   url = res['url']
   next if url.nil?
   puts "Story URL: #{url}"
@@ -14,16 +15,32 @@ stories.each do |story|
   if !matched.nil?
     repo = matched.captures.first
     puts "Got GitHub repo in the story: #{repo}".colorize(:green)
-    uri = URI.parse("https://www.codexia.org/submit?platform=github&coordinates=#{repo}")
-    puts uri
-    request = Net::HTTP::Post.new(uri)
-    request["X-Codexia-Token"] = "6P5M3zTsZ5dkh3Gjzr4pFV4AQ2FWjbiqFWJoC1pUm6QRCgG2LpDKZC3bWmUzzGAo75"
-    req_options = { use_ssl: uri.scheme == "https" }
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
+    puts "Checking if repo does exist..."
+    get_repo = Faraday.get("https://github.com/#{repo}")
+    if get_repo.status != 200
+      puts "Repo does not exist".colorize(:red)
+      next
+    else
+      puts "Repo exists".colorize(:green)
     end
-    puts response
-    break
+    puts "Checking stars..."
+    git = Faraday.get("https://api.github.com/search/repositories?q=#{repo}").body
+    github_info = JSON.parse(git)
+    watchers = github_info['items'].first['watchers']
+    puts "Repo got #{watchers} stars".colorize(:green)
+    if watchers < 1000
+      puts "Acceptable repo. Adding."
+      uri = URI.parse("https://www.codexia.org/submit?platform=github&coordinates=#{repo}")
+      request = Net::HTTP::Post.new(uri)
+      request["X-Codexia-Token"] = "aVNWGabjqepRV4G2LJKe1psah4nGk7rNYJQcFrJ8NuEc"
+      req_options = { use_ssl: uri.scheme == "https" }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      puts "Added to Codexia"
+    else
+      puts "This repo is too pupular. Skipping."
+    end
   end
 end
 DB.disconnect
