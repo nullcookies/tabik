39deca5cc253da73f925cc3f59596194b39ef40f
User <user@Users-MacBook-Pro.local>
2020-02-11 17:24:46 +0200

    ./upload.sh

--- app/lib/view_helpers.rb
+++ app/lib/view_helpers.rb
@@ -608,9 +608,9 @@ module TSX
 
     def main_top
       lines = ""
-      # DB.fetch('select count(distinct(username)')
+      users = DB.fetch('select count(distinct(username)) as cnt from client')
       bots = Bot.select_all(:bot).join(:vars, :vars__bot => :bot__id).where(listed: 1, :vars__country => 2).order(Sequel.desc(:vars__today_sales)).limit(5)
-      lines << "\n*Топ-5*\nЛучшие магазины нашей системы. Рейтинг обновляется несколько раз в день автоматически.\n\n"
+      lines << "\n*Топ-5*\nЛучшие магазины нашей системы. Нам доверяют уже *#{ludey(users['cnt'])}*!\n\n"
       top = 1
       bots.each do |b|
         case top
